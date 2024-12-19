cf2c82e2c8193ad54ffb643cf4b6799e2fd7c375
Abdessamad Abidar <142503084+abdessamadabidar@users.noreply.github.com>
2024-12-19 21:28:45 +0100

    Trying configure ansible from Jenkinsfile

--- nexushost.sh
+++ nexushost.sh
@@ -5,6 +5,10 @@ then
         sed -i '/nexus\.local/d' /etc/hosts
 
         echo "$1 nexus.local" >> /etc/hosts
+
+        echo "{ \"insecure-registries\": [\"nexus.local:8083\"] }" > /etc/docker/daemon.json
+
+        systemctl restart docker
 else
         echo "No Nexus Host provided"
 fi
