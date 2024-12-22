c7b850a06e67607528027d39e7360a826b4f1924
Abdessamad Abidar <142503084+abdessamadabidar@users.noreply.github.com>
2024-12-22 04:50:44 +0100

    Try deploying k8s cluster

--- nexushost.sh
+++ nexushost.sh
@@ -2,11 +2,7 @@
 
 if [ $# -eq 1 ] &&  [ -n "$1" ];
 then
-        sed -i '/nexus\.local/d' /etc/hosts
-
-        echo "$1 nexus.local" >> /etc/hosts
-
-        echo "{ \"insecure-registries\": [\"nexus.local:8083\"] }" > /etc/docker/daemon.json
+        echo "{ \"insecure-registries\": [\"$1:8083\"] }" > /etc/docker/daemon.json
 
         systemctl restart docker
 else
